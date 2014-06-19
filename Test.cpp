07d0021d1982ceb219181a7b5e46dfcc78642da0
Madscientist <mad@scientist.com>
2014-06-19 00:20:28 -0400

    More unit tests

--- Test.cpp
+++ Test.cpp
@@ -8,7 +8,7 @@
 #include <memory>
 
 #define TEST_PORT       258258
-#define TEST_TIMEOUT    2000
+#define TEST_TIMEOUT    1000
 
 using namespace std;
 
@@ -63,33 +63,60 @@ TEST ( SocketTest, ConnectTcpTimeout )
     EXPECT_FALSE ( client.socket->isConnected() );
 }
 
-// TEST ( SocketTest, SendTcpMessage )
-// {
-    // struct TestSocket : public Socket::Owner, public Timer::Owner
-    // {
-        // shared_ptr<Socket> socket, accept;
-        // bool connected;
+TEST ( SocketTest, SendTcpMessage )
+{
+    struct TestSocket : public Socket::Owner, public Timer::Owner
+    {
+        shared_ptr<Socket> socket, accepted;
+        MsgPtr msg;
 
-        // void acceptEvent ( Socket *serverSocket ) { accept = serverSocket->accept ( this ); connected = accept.get(); }
-        // void connectEvent ( Socket *socket ) { connected = socket->isConnected(); }
-        // void disconnectEvent ( Socket *socket ) { connected = false; }
+        void acceptEvent ( Socket *serverSocket )
+        {
+            accepted = serverSocket->accept ( this );
+            accepted->send ( TestMessage ( "Hello client!" ) );
+        }
 
-        // void timerExpired ( Timer *timer ) { EventManager::get().stop(); }
+        void connectEvent ( Socket *socket )
+        {
+            socket->send ( TestMessage ( "Hello server!" ) );
+        }
 
-        // TestSocket ( unsigned port )
-            // : socket ( Socket::listen ( this, port, Protocol::TCP ) ), connected ( false ) {}
-        // TestSocket ( const string& address, unsigned port )
-            // : socket ( Socket::connect ( this, address, port, Protocol::TCP ) ), connected ( false ) {}
-    // };
+        void readEvent ( Socket *socket, char *bytes, size_t len, const IpAddrPort& address )
+        {
+            msg = Serializable::decode ( bytes, len );
+        }
 
-    // TestSocket server ( TEST_PORT );
-    // TestSocket client ( "127.0.0.1", TEST_PORT );
+        void timerExpired ( Timer *timer ) { EventManager::get().stop(); }
 
-    // Timer timer ( &client );
-    // timer.start ( TEST_TIMEOUT );
+        TestSocket ( unsigned port )
+            : socket ( Socket::listen ( this, port, Protocol::TCP ) ) {}
+        TestSocket ( const string& address, unsigned port )
+            : socket ( Socket::connect ( this, address, port, Protocol::TCP ) ) {}
+    };
 
-    // EventManager::get().start();
+    TestSocket server ( TEST_PORT );
+    TestSocket client ( "127.0.0.1", TEST_PORT );
 
-    // EXPECT_TRUE ( server.connected );
-    // EXPECT_TRUE ( client.connected );
-// }
+    Timer timer ( &client );
+    timer.start ( TEST_TIMEOUT );
+
+    EventManager::get().start();
+
+    EXPECT_TRUE ( server.socket->isConnected() );
+    EXPECT_TRUE ( server.msg.get() );
+
+    if ( server.msg.get() )
+    {
+        EXPECT_EQ ( server.msg->type(), MsgType::TestMessage );
+        EXPECT_EQ ( server.msg->getAs<TestMessage>()->str, "Hello server!" );
+    }
+
+    EXPECT_TRUE ( client.socket->isConnected() );
+    EXPECT_TRUE ( client.msg.get() );
+
+    if ( client.msg.get() )
+    {
+        EXPECT_EQ ( client.msg->type(), MsgType::TestMessage );
+        EXPECT_EQ ( client.msg->getAs<TestMessage>()->str, "Hello client!" );
+    }
+}
