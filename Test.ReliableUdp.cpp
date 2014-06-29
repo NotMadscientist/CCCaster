1b4492e296c88e2017d11004b409fa45edd65198
Madscientist <mad@scientist.com>
2014-06-29 15:59:20 -0400

    Disable server disconnect test for UDP

--- Test.ReliableUdp.cpp
+++ Test.ReliableUdp.cpp
@@ -3,8 +3,15 @@
 #define PACKET_LOSS     90
 #define KEEP_ALIVE      ( 30 * 1000 )
 
-TEST_CONNECT ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 60 * 1000 )
+TEST_CONNECT                ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 60 * 1000 )
 
-TEST_TIMEOUT ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 1000 )
+TEST_TIMEOUT                ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 1000 )
 
-TEST_SEND    ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 120 * 1000 )
+TEST_CLIENT_DISCONNECT      ( ReliableUdp, Udp, 0, 0, 2000 )
+
+TEST_SERVER_DISCONNECT      ( ReliableUdp, Udp, 0, 0, 2000 )
+
+TEST_SEND                   ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 120 * 1000 )
+
+// This test doesn't make sense since there is only one UDP socket
+// TEST_SEND_WITHOUT_SERVER    ( ReliableUdp, Udp, PACKET_LOSS, KEEP_ALIVE, 120 * 1000 )
