5a764ab1dafa421f9961c25842bdf32bb85655c0
Madscientist <mad@scientist.com>
2014-07-11 14:41:54 -0400

    Ui stub files

---
+++ Ui.cpp
@@ -0,0 +1,9 @@
+#include "Ui.h"
+
+#include <JLib/ConsoleCore.h>
+
+using namespace std;
+
+// Initial console window dimensions (definition needed for JLIB)
+int MAXSCREENX = 80;
+int MAXSCREENY = 25;
